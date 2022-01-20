5eeee599eac1fee3470de2791d34510eb0b4235e
Raguf <ragufym@code.edu.az>
2022-01-21 01:55:16 +0400

    Add project files.

---
+++ Home.Work.№ 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Home.Work.__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ededin 4-e bolunub bolunmediyin alqoritmi yazmaq */
+
+            int a = 22;
+
+            int q = a % 4;
+
+            if (q == 0)
+
+            {
+                Console.WriteLine("Bolunur");
+            }
+
+            else
+
+            {
+                Console.WriteLine("Bolunmur");
+            }
+        }
+    }
+}
